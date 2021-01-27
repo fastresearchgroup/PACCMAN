b1757659db696135e1f69c034aa71cd78c85489c
hughfeehan353 <feehanh@gmail.com>
2021-01-27 14:15:35 -0500

    Update conformal-cooling.py

--- conformal-cooling.py
+++ conformal-cooling.py
@@ -16,41 +16,41 @@ savegraphs = input ("Would you like to save an image of the graphs? Y for yes, N
 
 #general note: when programfunction = N or H, the first choice of mold material properties is used (KM1, PM1, etc.)
 
-TC = 15.
+testTC = 15.
 #coolant temperature celsius
-PP = 980.
+testPP = 980.
 #density of plastic part kg/m^3
-CP = 1300.
+testCP = 1300.
 #specific heat capacity of plastic part J/KG*K
-LP = 0.001
+testLP = 0.001
 #half the plastic part thickness m
-W = 0.010
+testW = 0.010
 #cooling line pitch distance m
-D = 0.005
+testD = 0.005
 #cooling line diameter m
-LM = 0.004
+testLM = 0.004
 #distance from cooling line to mold wall
-TMelt = 180.
+testTMelt = 180.
 #Part melted temperature
-TEject = 64.9
+testTEject = 64.9
 #Part ejection temperature
-TCycle = 10.
+testTCycle = 10.
 #Cycle time seconds
-TMO = 13.
+testTMO = 13.
 #Initial mold temperature
-CVV = 0.227
+testCVV = 0.227
 #coolant velocity liters/sec
-DV = 1.002 * 10**-3
+testDV = 1.002 * 10**-3
 #coolant dynamic viscosity
-WDV = 0.0009775
+testWDV = 0.0009775
 #coolant dynamic viscosity when near wall
-KC = 0.5918
+testKC = 0.5918
 #thermal conductivity of coolant
-PC = 998.2
+testPC = 998.2
 #coolant density
-CC = 4187
+testCC = 4187
 #specific heat capacity of coolant
-L = 1.15
+testL = 1.15
 #coolant line length
 
 
@@ -60,209 +60,189 @@ moldmatname2 = "6061 Aluminum"
 #name of second mold material
 moldmatname3 = "Copper"
 #name of third mold material
-PM1 = 7930.
+test1PM = 7930.
 #First comparison Mold density kg/m^3: 316 steel
-PM2 = 2700
+test2PM = 2700
 #Second comparison Mold density kg/m^3: aluminum 6061
-PM3 = 8960
+test3PM = 8960
 #Third comparison Mold density kg/m^3: copper
-CM1 = 510.
+test1CM = 510.
 #First comparison Mold specific heat 316 steel
-CM2 = 896
+test2CM = 896
 #Second comparison Mold specific heat aluminum
-CM3 = 380
+test3CM = 380
 #Third comparison Mold specific heat copper
-fancye1 = 0.00015
+test1fancye = 0.00015
 #First comparison average height of pipe surface irregularities (m) 316 steel
-fancye2 =  0.000001
+test2fancye =  0.000001
 #Second comparison average height of pipe surface irregularities (m) aluminum
-fancye3 = 0.000001
+test3fancye = 0.000001
 #Third comparison average height of pipe surface irregularities (m) copper
-KM1 = 16.5
+test1KM = 16.5
 #First comparison thermal conductivity of mold: 316 steel
-KM2 = 180
+test2KM = 180
 #Second comparison thermal conductivity of mold: aluminum 6061
-KM3 = 402
+test3KM = 402
 #Third comparison thermal conductivity of mold: copper
 
 
 
 
-FV = (CVV*0.001)/(np.pi*(D/2)**2)
+def FVfunc(CVV, D): 
+    FV = (CVV*0.001)/(np.pi*(D/2)**2)
+    return FV
 #flow velocity of coolant m/s
 
-
-
-
-
+FV = FVfunc(testCVV, testD)
 print ("flow velocity:", FV)
 
-
-
-
-
-KV = DV/PC
+def KVfunc(DV,PC):
+    KV = DV/PC
+    return KV
 #coolant kinematic viscosity
 
 
 
-
+KV = KVfunc(testDV,testPC)
 print ("kinematic viscosity:", KV)
 
 
 
-
-
-RE = FV*D/KV
+def REfunc(FV,D,KV):
+    RE = FV*D/KV
+    return RE
 #Reynolds number of coolant
 
 
-
-
-
+RE = REfunc(FV,testD,KV)
 print ("Reynolds number:", RE)
 
 
-
-
-PR = DV*CC/KC
+def PRfunc(DV,CC,KC):
+    PR = DV*CC/KC
+    return PR
 #Prandl number of coolant
 
 
-
-
-
+PR = PRfunc(testDV,testCC,testKC)
 print ("Prandl number:", PR)
 
 
-
-
-
-DF1 = (1/(-1.8*np.log(((fancye1/(3.7*D))**1.11)+(6.9/RE))))**2
-
-DF2 = (1/(-1.8*np.log(((fancye2/(3.7*D))**1.11)+(6.9/RE))))**2
-
-DF3 = (1/(-1.8*np.log(((fancye3/(3.7*D))**1.11)+(6.9/RE))))**2
+def DFfunc(fancye,D,RE):
+    DF = (1/(-1.8*np.log(((fancye/(3.7*D))**1.11)+(6.9/RE))))**2
+    return DF
 #Darcy friction factor
 
-
-
-
-
+DF1 = DFfunc(test1fancye,testD,RE)
 print ("Darcy friction factor (",moldmatname1,"):", DF1)
 
-if programfunction == "M" or programfunction == "m":
+if programfunction == "M" or "m":
+    DF2 = DFfunc(test2fancye,testD,RE)
     print ("Darcy friction factor (",moldmatname2,"):", DF2)
+if programfunction == "M" or "m":
+    DF3 = DFfunc(test3fancye,testD,RE)   
     print ("Darcy friction factor (",moldmatname3,"):", DF3)
+    
 
-
-
-
-
-if programfunction == "N" or programfunction == "n":
-    if heat_coefficient_correlation == "D":
-        chosenNU1 = (0.023*RE**0.8)*PR**0.4
-    elif heat_coefficient_correlation == "G":
-        chosenNU1 = ((DF1/8)*(RE-1000)*PR)/(1+(12.7*((DF1/8)**0.5)*(PR**(2/3)-1)))
-    elif heat_coefficient_correlation == "S":
-        chosenNU1 = 0.027*(RE**(4/5))*(PR**(1/3))*((DV/WDV)**0.14)
-elif programfunction == "M" or programfunction == "m":
-    chosenNU1 = ((DF1/8)*(RE-1000)*PR)/(1+(12.7*((DF1/8)**0.5)*(PR**(2/3)-1)))
-    chosenNU2 = ((DF2/8)*(RE-1000)*PR)/(1+(12.7*((DF2/8)**0.5)*(PR**(2/3)-1)))
-    chosenNU3 = ((DF3/8)*(RE-1000)*PR)/(1+(12.7*((DF3/8)**0.5)*(PR**(2/3)-1)))
-elif programfunction == "H" or programfunction == "h":
-    chosenNU1 = (0.023*RE**0.8)*PR**0.4
-    chosenNU2 = ((DF1/8)*(RE-1000)*PR)/(1+(12.7*((DF1/8)**0.5)*(PR**(2/3)-1)))
-    chosenNU3 = 0.027*(RE**(4/5))*(PR**(1/3))*((DV/WDV)**0.14)
-else: 
-    print ("false")
-#chosen correlation to Nusselt number
-
-
-
-h1 = (KC/D)* chosenNU1
-
-if programfunction == "M" or programfunction == "m" or programfunction == "H" or programfunction == "h":
-    h2 = (KC/D)* chosenNU2
-    h3 = (KC/D)* chosenNU3
+def DBNU(RE,PR):
+    NU = (0.023*RE**0.8)*PR**0.4
+    return NU
+def GNU(DF,RE,PR):
+    NU = ((DF/8)*(RE-1000)*PR)/(1+(12.7*((DF/8)**0.5)*(PR**(2/3)-1)))
+    return NU
+def STNU(RE,PR,DV,WDV):
+    NU = 0.027*(RE**(4/5))*(PR**(1/3))*((DV/WDV)**0.14)
+    return NU
+	
+def htc(KC,D,NU):
+    h = (KC/D)* NU
+    return h
 #Heat transfer coefficient
 
 
 
-if programfunction == "M" or programfunction == "m": 
+if programfunction == "M" or "m": 
+    h1 = htc(testKC,testD,GNU(DF1,RE,PR))
     print ("heat transfer coefficient (",moldmatname1,"):", h1)
+    h2 = htc(testKC,testD,GNU(DF2,RE,PR))
     print ("heat transfer coefficient (",moldmatname2,"):", h2)
+    h3 = htc(testKC,testD,GNU(DF3,RE,PR))
     print ("heat transfer coefficient (",moldmatname3,"):", h3)
-elif programfunction == "H" or programfunction == "h":
+
+elif programfunction == "H" or "h":
+    h1 = htc(testKC,testD,DBNU(RE,PR))
     print ("heat transfer coefficient (Ditus-Boelter):", h1)
+    h2 = htc(testKC,testD,DBNU(RE,PR))
     print ("heat transfer coefficient (Gnielinski):", h2)
+    h3 = htc(testKC,testD,DBNU(RE,PR,testDV,testWDV))
     print ("heat transfer coefficient (Sieder-Tate):", h3)
-elif programfunction == "N" or programfunction == "n":
-    print ("heat transfer coefficient", h1)
+elif programfunction == "N" or "n":
+    if heat_coefficient_correlation == "D":
+        h1 = htc(testKC,testD,DBNU(RE,PR))
+        print ("heat transfer coefficient", h1)
+    elif heat_coefficient_correlation == "G":
+        h2 = htc(testKC,testD,GNU(DF1,RE,PR))
+        print ("heat transfer coefficient", h2)
+    elif heat_coefficient_correlation == "S":
+        h3 = htc(testKC,testD,DBNU(RE,PR,testDV,testWDV))
+        print ("heat transfer coefficient", h3)
+        
+else: 
+    print ("false")
 
 
 
-if programfunction == "M" or programfunction == "m": 
-    ATM1 = PP*CP*LP*(2.0*KM1*W + h1*D*LM*np.pi)*(TMelt - TEject)
-    ATM2 = PP*CP*LP*(2.0*KM2*W + h2*D*LM*np.pi)*(TMelt - TEject)
-    ATM3 = PP*CP*LP*(2.0*KM3*W + h3*D*LM*np.pi)*(TMelt - TEject)
-    
-    ATM1 = ATM1/(h1*D*KM1*TCycle*np.pi)
-    ATM2 = ATM2/(h2*D*KM2*TCycle*np.pi)
-    ATM3 = ATM3/(h3*D*KM3*TCycle*np.pi)
-    
-    ATM1 = ATM1 + TC
-    ATM2 = ATM2 + TC
-    ATM3 = ATM3 + TC
-    
+def ATMfunc(PP,CP,LP,KM,W,h,D,LM,TMelt,TEject,TCycle,TC):
+    ATM = PP*CP*LP*(2.0*KM*W + h*D*LM*np.pi)*(TMelt - TEject)
+    ATM = ATM/(h*D*KM*TCycle*np.pi) + TC
+    return ATM
+
+if programfunction == "M" or "m": 
+    ATM1 = ATMfunc(testPP,testCP,testLP,test1KM,testW,h1,testD,testLM,testTMelt,testTEject,testTCycle,testTC)
     print ("temperature of the mold (",moldmatname1,"):", ATM1)
+    ATM2 = ATMfunc(testPP,testCP,testLP,test2KM,testW,h2,testD,testLM,testTMelt,testTEject,testTCycle,testTC)
     print ("temperature of the mold (",moldmatname2,"):", ATM2)
+    ATM3 = ATMfunc(testPP,testCP,testLP,test3KM,testW,h3,testD,testLM,testTMelt,testTEject,testTCycle,testTC)
     print ("temperature of the mold (",moldmatname3,"):", ATM3)
-elif programfunction == "H" or programfunction == "h":
-    ATM1 = PP*CP*LP*(2.0*KM1*W + h1*D*LM*np.pi)*(TMelt - TEject)
-    ATM2 = PP*CP*LP*(2.0*KM1*W + h2*D*LM*np.pi)*(TMelt - TEject)
-    ATM3 = PP*CP*LP*(2.0*KM1*W + h3*D*LM*np.pi)*(TMelt - TEject)
+
+elif programfunction == "H" or "h":
     
-    ATM1 = ATM1/(h1*D*KM1*TCycle*np.pi) + TC
-    ATM2 = ATM2/(h2*D*KM1*TCycle*np.pi) + TC
-    ATM3 = ATM3/(h3*D*KM1*TCycle*np.pi) + TC
-    
+    ATM1 = ATMfunc(testPP,testCP,testLP,test1KM,testW,h1,testD,testLM,testTMelt,testTEject,testTCycle,testTC)
     print ("temperature of the mold (Ditus-Boelter):", ATM1)
+    ATM2 = ATMfunc(testPP,testCP,testLP,test1KM,testW,h2,testD,testLM,testTMelt,testTEject,testTCycle,testTC)
     print ("temperature of the mold (Gnielinski):", ATM2)
+    ATM3 = ATMfunc(testPP,testCP,testLP,test1KM,testW,h3,testD,testLM,testTMelt,testTEject,testTCycle,testTC)
     print ("temperature of the mold (Sieder-Tate):", ATM3)
-elif programfunction == "N" or programfunction == "n":
-    ATM1 = PP*CP*LP*(2.0*KM1*W + h1*D*LM*np.pi)*(TMelt - TEject)
     
-    ATM1 = ATM1/(h1*D*KM1*TCycle*np.pi)
-    
-    ATM1 = ATM1 + TC
-    
+elif programfunction == "N" or "n":
+    ATM1 = ATMfunc(testPP,testCP,testLP,test1KM,testW,h1,testD,testLM,testTMelt,testTEject,testTCycle,testTC)
     print ("temperature of the mold:", ATM1)
 #Average temperature of the mold
 
 
 
-if programfunction == "M" or programfunction == "m":
-    TConstant1 = ((PM1*CM1*LM**2)/KM1)*(1+(2.0*W*KM1)/(h1*D*LM*np.pi))
-    TConstant2 = ((PM2*CM2*LM**2)/KM2)*(1+(2.0*W*KM2)/(h2*D*LM*np.pi))
-    TConstant3 = ((PM3*CM3*LM**2)/KM3)*(1+(2.0*W*KM3)/(h3*D*LM*np.pi))
-elif programfunction == "H" or programfunction == "h":
-    TConstant1 = ((PM1*CM1*LM**2)/KM1)*(1+(2.0*W*KM1)/(h1*D*LM*np.pi))
-    TConstant2 = ((PM1*CM1*LM**2)/KM1)*(1+(2.0*W*KM1)/(h2*D*LM*np.pi))
-    TConstant3 = ((PM1*CM1*LM**2)/KM1)*(1+(2.0*W*KM1)/(h3*D*LM*np.pi))
-elif programfunction == "N" or programfunction == "n":
-    TConstant1 = ((PM1*CM1*LM**2)/KM1)*(1+(2.0*W*KM1)/(h1*D*LM*np.pi))
+def TConstantfunc(PM,CM,LM,KM,W,h,D):
+    TConstant = ((PM*CM*LM**2)/KM)*(1+(2.0*W*KM)/(h*D*LM*np.pi))
+    return TConstant
 
-if programfunction == "M" or programfunction == "m": 
+if programfunction == "M" or "m":
+    TConstant1 = TConstantfunc(test1PM,test1CM,testLM,test1KM,testW,h1,testD)
+    TConstant2 = TConstantfunc(test2PM,test2CM,testLM,test2KM,testW,h2,testD)
+    TConstant3 = TConstantfunc(test3PM,test3CM,testLM,test3KM,testW,h3,testD)
     print ("time constant (",moldmatname1,"):", TConstant1)
     print ("time constant (",moldmatname2,"):", TConstant2)
     print ("time constant (",moldmatname3,"):", TConstant3)
-elif programfunction == "H" or programfunction == "h":
+elif programfunction == "H" or "h":
+    TConstant1 = TConstantfunc(test1PM,test1CM,testLM,test1KM,testW,h1,testD)
+    TConstant2 = TConstantfunc(test1PM,test1CM,testLM,test1KM,testW,h2,testD)
+    TConstant3 = TConstantfunc(test1PM,test1CM,testLM,test1KM,testW,h3,testD)
     print ("time constant (Ditus-Boelter):", TConstant1)
     print ("time constant (Gnielinski):", TConstant2)
     print ("time constant (Sieder-Tate):", TConstant3)
-elif programfunction == "N" or programfunction == "n":
+elif programfunction == "N" or "n":
+    TConstant1 = TConstantfunc(test1PM,test1CM,testLM,test1KM,testW,h1,testD)
     print ("time constant", TConstant1)
+
 #Time constant
 
 
@@ -273,11 +253,11 @@ x = np.linspace(0,100)
 
 
 
-y1 = ATM1 + ((TMO-ATM1)*np.e**(-x/TConstant1))
+y1 = ATM1 + ((testTMO-ATM1)*np.e**(-x/TConstant1))
 if programfunction == "M" or programfunction == "m" or programfunction == "H" or programfunction == "h":
-    y2 = ATM2 + ((TMO-ATM2)*np.e**(-x/TConstant2))
+    y2 = ATM2 + ((testTMO-ATM2)*np.e**(-x/TConstant2))
 if programfunction == "M" or programfunction == "m" or programfunction == "H" or programfunction == "h":
-    y3 = ATM3 + ((TMO-ATM3)*np.e**(-x/TConstant3))
+    y3 = ATM3 + ((testTMO-ATM3)*np.e**(-x/TConstant3))
 
 
 
@@ -310,15 +290,21 @@ if savegraphs == "Y" or savegraphs == "y":
 
 
 
-if programfunction == "M" or programfunction == "m":
-    pdrop1 = (DF1*L/D)*(PC/2)*CVV**2
+def pdropfunc(DF,L,D,PC,CVV):
+    pdrop = (DF*L/D)*(PC/2)*CVV**2
+    return pdrop
+
+if programfunction == "M" or "m":
+    pdrop1 = pdropfunc(DF1,testL,testD,testPC,testCVV)
+    pdrop2 = pdropfunc(DF2,testL,testD,testPC,testCVV)
+    pdrop3 = pdropfunc(DF3,testL,testD,testPC,testCVV)
+
     print ("coolant pressure drop (",moldmatname1,"):", pdrop1)
-    pdrop2 = (DF2*L/D)*(PC/2)*CVV**2
     print ("coolant pressure drop (",moldmatname2,"):", pdrop2)
-    pdrop3 = (DF3*L/D)*(PC/2)*CVV**2
     print ("coolant pressure drop (",moldmatname3,"):", pdrop3)
-elif programfunction == "H" or programfunction == "h" or programfunction == "N" or programfunction == "n":
-    pdrop1 = (DF1*L/D)*(PC/2)*CVV**2
+
+elif programfunction == "H" or "h" or "N" or "n":
+    pdrop1 = pdropfunc(DF1,testL,testD,testPC,testCVV)
     print ("coolant pressure drop:", pdrop1)
 
 #coolant pressure drop
