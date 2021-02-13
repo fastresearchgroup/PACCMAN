b2cf359d2ff58ce7632a7916c373b3a3cca6145e
hughfeehan353 <feehanh@gmail.com>
2021-02-13 10:31:55 -0500

    Create test.py

---
+++ test.py
@@ -0,0 +1,48 @@
+testTC = 15.
+#coolant temperature celsius
+testPP = 980.
+#density of plastic part kg/m^3
+testCP = 1300.
+#specific heat capacity of plastic part J/KG*K
+testLP = 0.001
+#half the plastic part thickness m
+testW = 0.010
+#cooling line pitch distance m
+testD = 0.005
+#cooling line diameter m
+testLM = 0.004
+#distance from cooling line to mold wall
+testTMelt = 180.
+#Part melted temperature
+testTEject = 64.9
+#Part ejection temperature
+testTCycle = 10.
+#Cycle time seconds
+testTMO = 13.
+#Initial mold temperature
+testCVV = 0.227
+#coolant velocity liters/sec
+testDV = 1.002 * 10**-3
+#coolant dynamic viscosity
+testWDV = 0.0009775
+#coolant dynamic viscosity when near wall
+testKC = 0.5918
+#thermal conductivity of coolant
+testPC = 998.2
+#coolant density
+testCC = 4187
+#specific heat capacity of coolant
+testL = 1.15
+#coolant line length
+
+testPM = 7930.
+#First comparison Mold density kg/m^3: 316 steel
+
+testCM = 510.
+#First comparison Mold specific heat 316 steel
+
+testfancye = 0.00015
+#First comparison average height of pipe surface irregularities (m) 316 steel
+
+testKM = 16.5
+#First comparison thermal conductivity of mold: 316 steel
