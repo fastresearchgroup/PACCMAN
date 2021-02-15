63e8fc1a8d1bb2478d18d0875edda800e7ccb95d
hughfeehan353 <feehanh@gmail.com>
2021-02-15 12:30:14 -0500

    change varible names

--- conformal-cooling.py
+++ conformal-cooling.py
@@ -15,41 +15,41 @@ if programfunction == "N" or programfunction == "n":
 
 import test
 
-testTC = 15.
+TC = 15.
 #coolant temperature celsius
-testPP = 980.
+PP = 980.
 #density of plastic part kg/m^3
-testCP = 1300.
+CP = 1300.
 #specific heat capacity of plastic part J/KG*K
-testLP = 0.001
+LP = 0.001
 #half the plastic part thickness m
-testW = 0.010
+W = 0.010
 #cooling line pitch distance m
-testD = 0.005
+D = 0.005
 #cooling line diameter m
-testLM = 0.004
+LM = 0.004
 #distance from cooling line to mold wall
-testTMelt = 180.
+TMelt = 180.
 #Part melted temperature
-testTEject = 64.9
+TEject = 64.9
 #Part ejection temperature
-testTCycle = 10.
+TCycle = 10.
 #Cycle time seconds
-testTMO = 13.
+TMO = 13.
 #Initial mold temperature
-testCVV = 0.227
+CVV = 0.227
 #coolant velocity liters/sec
-testDV = 1.002 * 10**-3
+DV = 1.002 * 10**-3
 #coolant dynamic viscosity
-testWDV = 0.0009775
+WDV = 0.0009775
 #coolant dynamic viscosity when near wall
-testKC = 0.5918
+KC = 0.5918
 #thermal conductivity of coolant
-testPC = 998.2
+PC = 998.2
 #coolant density
-testCC = 4187
+CC = 4187
 #specific heat capacity of coolant
-testL = 1.15
+L = 1.15
 #coolant line length
 
 moldmatname1 = "316 Steel" 
@@ -58,29 +58,29 @@ moldmatname2 = "6061 Aluminum"
 #name of second mold material
 moldmatname3 = "Copper"
 #name of third mold material
-test1PM = 7930.
+mat1PM = 7930.
 #First comparison Mold density kg/m^3: 316 steel
-test2PM = 2700
+mat2PM = 2700
 #Second comparison Mold density kg/m^3: aluminum 6061
-test3PM = 8960
+mat3PM = 8960
 #Third comparison Mold density kg/m^3: copper
-test1CM = 510.
+mat1CM = 510.
 #First comparison Mold specific heat 316 steel
-test2CM = 896
+mat2CM = 896
 #Second comparison Mold specific heat aluminum
-test3CM = 380
+mat3CM = 380
 #Third comparison Mold specific heat copper
-test1fancye = 0.00015
+mat1fancye = 0.00015
 #First comparison average height of pipe surface irregularities (m) 316 steel
-test2fancye =  0.000001
+mat2fancye =  0.000001
 #Second comparison average height of pipe surface irregularities (m) aluminum
-test3fancye = 0.000001
+mat3fancye = 0.000001
 #Third comparison average height of pipe surface irregularities (m) copper
-test1KM = 16.5
+mat1KM = 16.5
 #First comparison thermal conductivity of mold: 316 steel
-test2KM = 180
+mat2KM = 180
 #Second comparison thermal conductivity of mold: aluminum 6061
-test3KM = 402
+mat3KM = 402
 #Third comparison thermal conductivity of mold: copper
 
 def FVfunc(CVV, D): 
